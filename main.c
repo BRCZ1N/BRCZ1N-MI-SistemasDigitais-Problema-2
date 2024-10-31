041db19690ca99924926d0020b992325056fa40e
BRCZ1N <brunocamposo.r@gmail.com>
2024-10-31 16:58:49 -0300

    Refazendo o diretorio

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-extern int dot_product(int a, int b, int c, int d, int e    );
-
-int main(){
-    int x = 5,y=3, z=1,w=4, h=5;
-    int result = dot_product(x,y,z,w,h);
-    printf("%d", result);
-    return 0;
-}
