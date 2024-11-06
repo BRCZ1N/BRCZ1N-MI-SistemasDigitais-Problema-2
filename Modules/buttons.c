aa3a806e25198688b7b80623a09e8ed4fadf847b
BRCZ1N <brunocamposo.r@gmail.com>
2024-11-06 12:54:43 -0300

    Removendo makefile extra, Ajustando Gpu Lib e removendo bitmap

--- Modules/buttons.c
+++ Modules/buttons.c
@@ -1,4 +1,76 @@
 #include "prototype.h"
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#define KEYS_BASE 0x0
+#define ALT_LWFPGASLVS_OFST 0xff200000
+#define HW_REGS_BASE 0xfc000000
+#define HW_REGS_SPAN 0x04000000
+
+// Variáveis globais para o mapeamento e o arquivo de memória
+int fd;
+void *virtual_base;
+volatile uint32_t *keys_ptr;
+
+// Função para abrir e mapear a memória dos botões
+int button_open() {
+    // Abre o dispositivo de memória
+    fd = open("/dev/mem", O_RDWR | O_SYNC);
+    if (fd == -1) {
+        printf("Erro ao abrir /dev/mem\n");
+        return -1;
+    }
+
+    // Mapeia o endereço base
+    virtual_base = mmap(NULL, HW_REGS_SPAN, PROT_READ | PROT_WRITE, MAP_SHARED, fd, HW_REGS_BASE + ALT_LWFPGASLVS_OFST);
+    if (virtual_base == MAP_FAILED) {
+        printf("Erro no mmap\n");
+        close(fd);
+        return -1;
+    }
+
+    // Define o ponteiro para o endereço dos botões
+    keys_ptr = (uint32_t *) (virtual_base + KEYS_BASE);
+    return 0;
+}
+
+// Função para ler o estado dos botões
+uint32_t button_read() {
+    if (keys_ptr == NULL) {
+        printf("Erro: ponteiro para os botões não inicializado\n");
+        return -1;
+    }
+    return *keys_ptr;
+}
+
+// Função para fechar e liberar o mapeamento de memória
+void button_close() {
+    if (munmap(virtual_base, HW_REGS_SPAN) != 0) {
+        printf("Erro ao desmapear\n");
+    }
+    close(fd);
+}
+
+// int main() {
+//     // Abre o mapeamento de memória
+//     if (button_open() != 0) {
+//         return -1;
+//     }
+
+//     // Lê o valor dos botões
+//     uint32_t button_value = button_read();
+//     printf("Valor dos push buttons: 0x%x\n", button_value);
+
+//     // Fecha e libera o mapeamento de memória
+//     button_close();
+    
+//     return 0;
+// }
+
+
 
 /**
  * Altera o estado de pausa do jogo com base na entrada dos botões.
@@ -48,14 +120,14 @@ void changePauseState(int *pointerStateGame, int *pointerButtons)
  *              Um valor específico indica um botão específico.
  */
 
-int buttonRead()
-{
+// int buttonRead()
+// {
 
-    int readValue;
+//     int readValue;
 
-    KEY_open();
-    KEY_read(&readValue);
-    KEY_close();
+//     KEY_open();
+//     KEY_read(&readValue);
+//     KEY_close();
 
-    return readValue;
-}
+//     return readValue;
+// }
